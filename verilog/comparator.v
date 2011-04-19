c0bdaeb155769c4de8116b5208b061d4b9affe3a
Danny Peters <danmanstx@gmail.com>
2011-04-19 19:18:41 -0400

    added comparator verilog file

---
+++ verilog/comparator.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    18:14:22 04/19/2011 
+// Design Name: 
+// Module Name:    comparator 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module comparator(A, B, En, out);
+    parameter width = 8;
+    input [width-1:0]A;
+    input [width-1:0]B;
+    input En;
+    output out;
+	 reg out;
+	 
+	always @ (A or B or En )
+	begin
+		if(En)
+		begin
+			assign out = A >= B ? 1'b1 :1'b0;
+		end
+		else
+		begin
+			assign out = 0;
+		end
+	end
+
+endmodule
