c235909b03a856b722be912676808ade2ba73abd
John Wright <jwright6323@gmail.com>
2011-04-20 05:01:33 -0400

    cache is written. testing TODO

--- verilog/cache.v
+++ verilog/cache.v
@@ -15,60 +15,157 @@ module cache(addr_in, data_in, rw_in, ce_in, addr_out, data_out, rw_out, ce_out,
     //parameters
     parameter d_width = 8;      //data bus width
     parameter a_width = 8;      //address line width
-    
+
     //inputs to the cache
     input [a_width-1:0] addr_in;        //address in
+    reg [a_width-1:0] addr_in_reg;      //register that stores address in
     inout [d_width-1:0] data_in;        //data bus into the cache
     reg [d_width-1:0] data_in_reg;      //data in register (for output)
     input rw_in;                        //read if rw=1 write if rw=0
     input ce_in;                        //chip enable (func. when ce=1, HiZ when ce=0)
-    
+
     //outputs to RAM
     output reg [a_width-1:0] addr_out;  //address out
     inout [d_width-1:0] data_out;       //data bus from the cache
     reg [d_width-1:0] data_out_reg;     //data out register (for output)
     output reg rw_out;                  //pass from input
     output reg ce_out;                  //pass from output
-    
-    //control inputs/outputs
-    output odv;                     //output data valid
+    output reg odv;                     //output data valid
+
+    //control inputs
     input clr;                      //synchronous active low clear
     input clk;                      //clock
-    
+
     //cache entries
     reg [3:0] valid;                //entry valid
     reg [1:0] cnt [3:0];            //count (for LRU replacement)
     reg [d_width-1:0] data [3:0];   //entry data
     reg [a_width-1:0] addr [3:0];   //entry addresses
-    
-    //other registers
+
+    //other registers and wires
     reg [2:0] timer;                //timer for a miss
-    integer hit;                        //hit=1 for a hit, hit=0 for a miss
-    integer i;  
-    
+    wire hit;                       //hit=1 for a hit, hit=0 for a miss
+    wire [1:0] sel;                 //sel=i if hit at entry i
+    reg [1:0] sel_reg;              //register to store sel between ccs
+    wire [3:0] dec;                 //dec[i]=1 if decrementing cnt[i]
+    integer i;
+
+    //modules
+    determine_hit DETERMINE_HIT (addr_in, addr, cnt, valid, sel, dec, hit);
+
     always@(posedge clk)
     begin
-        ce_out <= ce_in;
-        rw_out <= rw_in;
-        if(clr == 0)                //clear contents
+        if(clr == 1'b0)             //clear contents
         begin
-            data_out_reg <= 0;
-            data_in_reg <= 0;
-            timer <= 0;
+            data_out_reg <= 1'b0;
+            data_in_reg <= 1'b0;
+            timer <= 1'b0;
             for(i = 0; i < 3; i = i+1)
             begin
-                valid[i] <= 0;
-                cnt[i] <= 0;
-                data[i] <= 0;
-                addr[i] <= 0;
+                valid[i] <= 1'b0;
+                cnt[i] <= 1'b0;
+                data[i] <= 1'b0;
+                addr[i] <= 1'b0;
             end
         end
-        if(ce_in == 1)              //only read or write if chip is enabled
+        if(ce_in == 1'b1)           //only read or write if chip is enabled
         begin
-            if(clr == 1)
-            begin
-            hit <= 1;
-            end
+            case(timer)             //timer acts as FSM for cache
+                3'b000:
+                begin
+                    if(hit == 1'b1)     //hit
+                    begin
+                        if(rw == 1'b0)  //write
+                            data[sel] <= data_in;
+                        else            //read
+                            data_in_reg <= data[sel];
+                        cnt[sel] <= 2'b11;
+                        ce_out <= 1'b0;
+                        odv <= 1'b1;    //data is valid
+                        timer <= 3'b000;
+                    end
+                    else            //miss
+                    begin
+                        if(valid[sel] == 1'b1)
+                        begin
+                            rw_out <= 1'b0;
+                            data_out_reg <= data[sel];
+                            addr_out <= addr[sel];
+                            ce_out <= 1'b1;
+                        end
+                        else
+                            ce_out <= 1'b0;
+                        data_in_reg <= data_in;
+                        sel_reg <= sel;
+                        addr_in_reg <= addr_in;
+                        odv <= 1'b0;
+                        timer <= 3'b001;
+                    end
+                end
+                3'b001:
+                begin
+                    //perform read or write for miss
+                    if(rw == 1'b0)  //write
+                    begin
+                        data[sel_reg] <= data_in_reg;
+                    end
+                    else            //read
+                    begin
+                        data_in_reg <= data[sel_reg];
+                    end
+                    cnt[sel_reg] <= 2'b11;
+                    valid[sel_reg] <= 1'b1;
+                    addr_out <= addr_in_reg;
+                    ce_out <= 1'b1;
+                    odv <= 1'b0;
+                    timer <= 3'b010;
+                end
+                3'b010:
+                begin
+                    ce_out <= 1'b0;
+                    odv <= 1'b0;
+                    timer <= 3'b011;
+                end
+                3'b011:
+                begin
+                    ce_out <= 1'b0;
+                    odv <= 1'b0;
+                    timer <= 3'b100;
+                end
+                3'b100:
+                begin
+                    ce_out <= 1'b0;
+                    odv <= 1'b0;
+                    timer <= 3'b101;
+                end
+                3'b101:
+                begin
+                    ce_out <= 1'b0;
+                    odv <= 1'b0;
+                    timer <= 3'b110;
+                end
+                3'b110:
+                begin
+                    ce_out <= 1'b0;
+                    odv <= 1'b0;
+                    timer <= 3'b111;
+                end
+                3'b111:
+                begin
+                    ce_out <= 1'b0;
+                    odv <= 1'b1;
+                    timer <= 3'b000;
+                end
+                default:
+                    ce_out <= 1'b0;
+                    odv <= 1'b0;
+                    timer <= 3'b000;
+            endcase
+        end
+        else        //chip is disabled
+        begin
+            ce_out <= 1'b0;
+            odv <= 1'b0;
         end
     end
 
@@ -77,14 +174,14 @@ module cache(addr_in, data_in, rw_in, ce_in, addr_out, data_out, rw_out, ce_out,
     genvar j;
 
     assign in_bufctrl = (rw_in && ce_in);       //buffer only when rw_in=1 and ce_in=1
-    assign out_bufctrl = (~rw_in && ce_in);     //buffer only when rw_in=0 and ce_in=1
-    
+    assign out_bufctrl = (~rw_out && ce_in);     //buffer only when rw_out=0 and ce_in=1
+
     //data direction buffers for inout
     generate
     for(j = 0; j < d_width; j = j+1)
     begin:buffers
-        bufif1 RW_BUF_IN (data_in[j],data_in_reg[j],in_bufctrl);
-        bufif1 RW_BUF_OUT (data_out[j],data_out_reg[j],out_bufctrl);
+        bufif1 RW_BUF_IN (data_in[j], data_in_reg[j], in_bufctrl);
+        bufif1 RW_BUF_OUT (data_out[j], data_out_reg[j], out_bufctrl);
     end
     endgenerate
 
--- verilog/determine_hit.v
+++ verilog/determine_hit.v
@@ -10,49 +10,125 @@
 //A module that is used in cache.v to choose which counters to decrement on a cache hit
 //
 ///////////////////////////////////////////////////////////////////////////////////////
-module determine_hit(addr, entry_addrs, valid, sel, dec, hit);
+module determine_hit(addr, entry_addrs, cnt, valid, sel, dec, hit);
     //parameters
     parameter d_width = 8;                  //data bus width
     parameter a_width = 8;                  //address line width
     //inputs
     input [a_width-1:0] addr;               //input address
     input [a_width-1:0] entry_addrs [3:0];  //entry address values
+    input [1:0] cnt [3:0];                  //entry counts
     input [3:0] valid;                      //entry valid values
     //outputs
     output [1:0] sel;                       //sel=i if hit at entry i, else hiZ
-    output [3:0] dec;                       //inc[i]=1 if decrementing cnt[i]
+    output [3:0] dec;                       //dec[i]=1 if decrementing cnt[i]
     output hit;                             //hit=1 if hit, hit=0 if miss
-    
+
     always@(addr or entry_addrs or valid)
     begin
-        else if((addr == entry_addrs[0]) && valid[0] == 1'b1)
+        else if((addr == entry_addrs[0]) && (valid[0] == 1'b1))
         begin
             //hit on entry 0
-            hit = 1;
+            hit = 1'b1;
             sel = 2'b00;
+            dec[0] = 1'b0;
+            if(cnt[0] > cnt[1])
+                dec[1] = 1'b0;
+            else
+                dec[1] = 1'b1;
+            if(cnt[0] > cnt[2])
+                dec[2] = 1'b0;
+            else
+                dec[2] = 1'b1;
+            if(cnt[0] > cnt[3])
+                dec[3] = 1'b0;
+            else
+                dec[3] = 1'b1;
         end
-        else if((addr == entry_addrs[1]) && valid[1] == 1'b1)
+        else if((addr == entry_addrs[1]) && (valid[1] == 1'b1))
         begin
             //hit on entry 1
-            hit = 1;
+            hit = 1'b1;
             sel = 2'b01;
+            dec[1] = 1'b0;
+            if(cnt[1] > cnt[0])
+                dec[0] = 1'b0;
+            else
+                dec[0] = 1'b1;
+            if(cnt[1] > cnt[2])
+                dec[2] = 1'b0;
+            else
+                dec[2] = 1'b1;
+            if(cnt[1] > cnt[3])
+                dec[3] = 1'b0;
+            else
+                dec[3] = 1'b1;
         end
-        else if((addr == entry_addrs[2]) && valid[2] == 1'b1)
+        else if((addr == entry_addrs[2]) && (valid[2] == 1'b1))
         begin
             //hit on entry 2
-            hit = 1;
+            hit = 1'b1;
             sel = 2'b10;
+            dec[2] = 1'b0;
+            if(cnt[2] > cnt[0])
+                dec[0] = 1'b0;
+            else
+                dec[0] = 1'b1;
+            if(cnt[2] > cnt[1])
+                dec[1] = 1'b0;
+            else
+                dec[1] = 1'b1;
+            if(cnt[2] > cnt[3])
+                dec[3] = 1'b0;
+            else
+                dec[3] = 1'b1;
         end
-        else if((addr == entry_addrs[3]) && valid[3] == 1'b1)
+        else if((addr == entry_addrs[3]) && (valid[3] == 1'b1))
         begin
             //hit on entry 3
-            hit = 1;
+            hit = 1'b1;
             sel = 2'b11;
+            dec[3] = 1'b0;
+            if(cnt[3] > cnt[0])
+                dec[0] = 1'b0;
+            else
+                dec[0] = 1'b1;
+            if(cnt[3] > cnt[1])
+                dec[1] = 1'b0;
+            else
+                dec[1] = 1'b1;
+            if(cnt[3] > cnt[2])
+                dec[2] = 1'b0;
+            else
+                dec[2] = 1'b1;
         end
-        else    //miss
+        else
         begin
-            hit = 0;
-            sel = 2'bzz;
+            //miss
+            if((cnt[0] == 2'b00) || (valid[0] == 1'b0))
+            begin
+                hit = 1'b0;
+                sel = 2'b00;
+                dec = 4'b0111;
+            end
+            else if((cnt[1] == 2'b00) || (valid[1] == 1'b0))
+            begin
+                hit = 1'b0;
+                sel = 2'b01;
+                dec = 4'b1011;
+            end
+            else if((cnt[2] == 2'b00) || (valid[2] == 1'b0))
+            begin
+                hit = 1'b0;
+                sel = 2'b10;
+                dec = 4'b1101;
+            end
+            else    //replace 3
+            begin
+                hit = 1'b0;
+                sel = 2'b11;
+                dec = 4'b1110;
+            end
         end
     end
 
