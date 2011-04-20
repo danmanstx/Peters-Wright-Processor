c7b1432cda599a5d274175651821309b29cfa0ba
Danny Peters <danmanstx@gmail.com>
2011-04-19 21:22:10 -0400

    added processor verilog file

---
+++ verilog/sign_extend.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    20:41:17 04/19/2011 
+// Design Name: 
+// Module Name:    sign_extend 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module sign_extend(in, out);
+	 input [5:0]in;
+    output[7:0]out;
+
+	assign out[0] = in[0];
+	assign out[1] = in[1];
+	assign out[2] = in[2];
+	assign out[3] = in[3];
+	assign out[4] = in[4];
+	assign out[5] = in[5];
+	assign out[6] = in[5];
+	assign out[7] = in[5];
+
+endmodule
