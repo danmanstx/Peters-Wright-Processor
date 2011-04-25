44f359c7802aa0583c5a924203541bb898f006eb
John Wright <jwright6323@gmail.com>
2011-04-25 01:54:39 -0400

    2am leaving OHR

--- verilog/testbench_cache.v
+++ verilog/testbench_cache.v
@@ -7,29 +7,44 @@
 //
 //testbench_cache.v
 //
-//A test bench for testing the cache functionality.  Not used in final design.
+//A testbench for the cache module
 //
 ///////////////////////////////////////////////////////////////////////////////////////
-module testbench_cache(clr, clk);
-    parameter d_width = 4;          //data bus width
-    parameter a_width = 8;          //address line width
-    input clr;                      //clear
-    input clk;                      //clock
-    
-    //wires----------------------------------------------------------------------------------------------------------------------------------------
-    wire [d_width-1:0] data_to_c;
-    wire [a_width-1:0] addr_to_r;
-    wire [d_width-1:0] data_to_r;
-    wire ce_to_r;
-    wire rw_to_r;
-    
-    //modules--------------------------------------------------------------------------------------------------------------------------------------
-    //module cache(addr_in, data_in, rw_in, ce_in, addr_out, data_out, rw_out, ce_out, odv, clr, clk);
-    cache #(.d_width(d_width),.a_width(a_width))       T_cache (addr, data_to_c, rw, ce, addr_to_r, data_to_r, rw_to_r, ce_to_r, odv, clr, clk);
-    
-    //module RAM(addr, ce, clk, clr, rw, data);
-    RAM #(.d_width(d_width),.a_width(a_width))         T_RAM (addr_to_r, ce_to_r, clk, clr, rw_to_r, data_to_r);
+module testbench_cache(clk, clr);
+    //parameters
+    parameter d_width = 8;      //data bus width
+    parameter a_width = 8;      //address line width
 
+    //inputs
+    input clk;                  //clock
+    input clr;                  //clear
+
+    //connection wires to RAM
+    wire [a_width-1:0] addr_toram;
+    wire [d_width-1:0] data_toram;
+    wire ce_toram;
+    wire rw_toram;
+
+    //connection wires and regs to cache
+    wire odv;
+    reg [a_width-1:0] addr_tocache;
+    reg [d_width-1:0] data_tocache;
+    reg rw_tocache;
+    reg ce_tocache;
+    parameter [7:0] testvectors = {8'b00000001,8'b00000010,8'b00000100,8'b00001000,8'b00010000,8'b00100000,8'b01000000,8'b10000000};
+
+    RAM   #(.d_width(d_width),.a_width(a_width)) dataram   (addr_toram, ce_toram, clk, clr, rw_toram, data_toram);
+    cache #(.d_width(d_width),.a_width(a_width)) datacache (addr_tocache , data_tocache,rw_tocache ,ce_tocache, addr_toram, data_toram, rw_toram, ce_toram, odv, clr, clk);
+    
+    always@(posedge clk)
+    begin
+        
+    end
+    
+    
+    
+    
+    
     
     
 endmodule
