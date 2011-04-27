a30dc294be4d9b8b238ea69865e0f0211f355f69
John Wright <jwright6323@gmail.com>
2011-04-27 19:20:33 -0400

    dumb error

--- assembler/run.sh
+++ assembler/run.sh
@@ -5,33 +5,34 @@
 #EE480 Spring 2011
 #DV Final Project
 ###########################################################
-set isr0at="@200"
-set isr1at="@210"
-set isr2at="@220"
-set isr3at="@230"
 echo "assembling interrupt service routines..."
 ./assembler z.bin isrs/z.txt
 ./assembler v.bin isrs/v.txt
 ./assembler opcode.bin isrs/opcode.txt
 ./assembler ext_int.bin isrs/ext_int.txt
-echo $isr0at > isrcode/isr0
+echo "@200" > isrcode/isr0
 cat v.bin >> isrcode/isr0
-echo $isr1at > isrcode/isr1
+echo "@210" > isrcode/isr1
 cat v.bin >> isrcode/isr1
-echo $isr2at > isrcode/isr2
+echo "@220" > isrcode/isr2
 cat v.bin >> isrcode/isr2
-echo $isr3at > isrcode/isr3
+echo "@230" > isrcode/isr3
 cat v.bin >> isrcode/isr3
 
 echo "building test program: interrupts..."
 ./assembler -mi interrupts.asm testprogs/interrupts.txt
+mv out_debug.txt interrupts_debug.txt
 echo "building test program: subroutine..."
 ./assembler -mi subroutine.asm testprogs/subroutine.txt
+mv out_debug.txt subroutine_debug.txt
 echo "building test program: bubblesort..."
 ./assembler -mi bubblesort.asm testprogs/bubblesort.txt
+mv out_debug.txt bubblesort_debug.txt
 echo "building test program: straightflow..."
 ./assembler -mi straightflow.asm testprogs/straightflow.txt
+mv out_debug.txt straightflow_debug.txt
 echo "building test program: fibonacci..."
 ./assembler -mi fibonacci.asm testprogs/fibonacci.txt
-mv *.v ../verilog
+mv out_debug.txt fibonacci_debug.txt
+#mv *.v ../verilog
 
